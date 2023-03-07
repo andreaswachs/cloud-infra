12878eb18d86c4b95c5d1f9683a91aef9de225b5
Andreas Wachs <andreas@wachs.dk>
2023-03-07 14:16:51 +0100

    Update delete script

--- delete.sh
+++ delete.sh
@@ -5,6 +5,4 @@ helm uninstall blog
 helm uninstall cert-manager -n cert-manager
 
 # Manually remove resourecs that were 'directly' applied
-kubectl delete -f https://github.com/cert-manager/cert-manager/releases/download/v1.10.1/cert-manager.crds.yaml
 kubectl delete -f cluster/issuers.yaml
-kubectl delete -f cluster/traefik-middleware.yaml
