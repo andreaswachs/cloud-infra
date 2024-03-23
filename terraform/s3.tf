8db7f1536cce09238bca3f009f5189289ff36941
Andreas Wachs <andreas@wachs.dk>
2024-03-23 16:36:52 +0100

    Bulk upload secret values to s3

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,4 +1,17 @@
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "k8s-app-secrets"
+  
+  force_destroy = true
+}
+
+# Uplad all files in the "../secrets" directory to the S3 bucket
+resource "aws_s3_object" "my_bucket_objects" {
+  for_each = fileset("../secrets", "**/*")
+
+  force_destroy = true
+
+  bucket = aws_s3_bucket.my_bucket.bucket
+  key    = each.value
+  source = "../secrets/${each.value}"
 }
 
