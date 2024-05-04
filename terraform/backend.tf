12ff3aca2cf3278476a14f35e6acdae36477038c
Andreas Wachs <andreas@wachs.dk>
2024-05-04 12:20:38 +0200

    Removes shared_credentials_file setting

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket                  = "tf-state-files-remote-backend"
     key                     = "cloud-infra"
     region                  = "eu-north-1"
-    shared_credentials_file = "~/.aws/credentials"
   }
 }
 
