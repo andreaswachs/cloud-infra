845ef3d6f476f50830420d2c15e007d0c014af14
Andreas Wachs <andreas@wachs.dk>
2024-05-05 16:02:06 +0200

    Add Vault provider

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,14 @@
 terraform {
   backend "s3" {
-    bucket                  = "tf-state-files-remote-backend"
-    key                     = "cloud-infra"
-    region                  = "eu-north-1"
+    bucket = "tf-state-files-remote-backend"
+    key    = "cloud-infra"
+    region = "eu-north-1"
+  }
+
+  required_providers {
+    vault = {
+      source  = "hashicorp/vault"
+      version = "~> 4.2"
+    }
   }
 }
-
--- terraform/main.tf
+++ terraform/main.tf
@@ -3,4 +3,7 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-
+provider "vault" {
+  address = "https://vault.wachs.work"
+  token   = var.vault_root_token
+}
