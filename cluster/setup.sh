8bf41080d7e6ed1f8c1c5df2c50df8d9072e10ce
Andreas Wachs <andreas@wachs.dk>
2024-08-25 22:33:21 +0200

    edit

--- cluster/setup.sh
+++ cluster/setup.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
-helm repo add jetstack https://charts.jetstack.io
-helm repo update
+IP="100.42.188.3"
+USER="host"
 
-helm upgrade \
-  cert-manager jetstack/cert-manager \
-  --install --atomic --wait --timeout=5m \
-  --namespace cert-manager \
-  --create-namespace \
-  --version v1.12.2 \
-  --set installCRDs=true
+k3sup install --ip $IP --user host --k3s-extra-args '--disable traefik'
 
-kubectl apply -f cluster/issuers.yaml
+
+
+# helm repo add jetstack https://charts.jetstack.io
+# helm repo update
+# 
+# helm upgrade \
+#   cert-manager jetstack/cert-manager \
+#   --install --atomic --wait --timeout=5m \
+#   --namespace cert-manager \
+#   --create-namespace \
+#   --version v1.12.2 \
+#   --set installCRDs=true
+# 
+# kubectl apply -f cluster/issuers.yaml
 
 # Setting up local storage
-cat << EOF | kubectl apply -f -
-kind: StorageClass
-apiVersion: storage.k8s.io/v1
-metadata:
-  name: local-storage
-provisioner: kubernetes.io/no-provisioner
-volumeBindingMode: WaitForFirstConsumer
-EOF
+#cat << EOF | kubectl apply -f -
+# kind: StorageClass
+# apiVersion: storage.k8s.io/v1
+# metadata:
+#   name: local-storage
+# provisioner: kubernetes.io/no-provisioner
+# volumeBindingMode: WaitForFirstConsumer
+# EOF
